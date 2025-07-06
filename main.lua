9e732f90f16733f827766ac7f473a9d974293d8f
Valentin Roussel <valentin.rssl@gmail.com>
2025-07-06 19:31:43 +0200

    add pipe generation

--- main.lua
+++ main.lua
@@ -1,7 +1,9 @@
 local love = require("love")
 local push = require("push")
 local Bird = require("Bird")
+local Pipe = require("Pipe")
 local keypressed = require("keypressed")
+local Timer = require("Timer")
 
 GAME_WIDTH = 432
 GAME_HEIGHT = 243
@@ -12,6 +14,8 @@ local WINDOW_HEIGHT = 720
 local BACKGROUND_SCROLL_SPEED = 30
 local GROUND_SCROLL_SPEED = 60
 local BACKGROUND_LOOPING_POINT = 413
+
+local PIPES_SPAWN_DELAY = 2
 
 local background_scroll = 0
 local ground_scroll = 0
@@ -24,18 +28,24 @@ local ground_img
 ---@type Bird
 local bird
 
+---@type table
+local pipes = {}
+---@type Timer
+local pipes_spawn_timer
+
 function love.load()
 	love.graphics.setDefaultFilter("nearest", "nearest")
 
 	background_img = love.graphics.newImage("assets/background.png")
 	ground_img = love.graphics.newImage("assets/ground.png")
 
-	love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, { resizable = true, vsync = true, fullscreen = false })
+	love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, { resizable = false, vsync = true, fullscreen = false })
 	push.setupScreen(GAME_WIDTH, GAME_HEIGHT, { upscale = "normal" })
 
 	math.randomseed(os.time())
 
 	bird = Bird:new()
+	pipes_spawn_timer = Timer:new(PIPES_SPAWN_DELAY)
 
 	love.window.setTitle("Flappy Bird")
 end
@@ -51,11 +61,37 @@ function love.keypressed(key, code)
 	end
 end
 
+local function spawn_pipes(dt)
+	pipes_spawn_timer:update(dt)
+	if pipes_spawn_timer:finished() then
+		table.insert(pipes, Pipe:new())
+		pipes_spawn_timer:reset()
+	end
+end
+
+local function cleanup_pipes()
+	for i, p in ipairs(pipes) do
+		if p.x + p.width < 0 then
+			table:remove(i)
+		else
+			-- Pipes are ordered so we can break as soon as we encounter an "on-screen" one
+			break
+		end
+	end
+end
+
 function love.update(dt)
 	background_scroll = (background_scroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
 	ground_scroll = (ground_scroll + GROUND_SCROLL_SPEED * dt) % GAME_WIDTH
 
 	bird:update(dt)
+
+	cleanup_pipes()
+	spawn_pipes(dt)
+
+	for _, p in ipairs(pipes) do
+		p:update(dt)
+	end
 end
 
 function love.draw()
@@ -65,6 +101,10 @@ function love.draw()
 	love.graphics.draw(ground_img, 0 - ground_scroll, GAME_HEIGHT - ground_img:getHeight())
 
 	bird:render()
+
+	for _, p in ipairs(pipes) do
+		p:render()
+	end
 
 	push.finish()
 end

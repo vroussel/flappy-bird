d4ca7de8b9b0f8af6496f6b59922cabef362560e
Valentin Roussel <valentin.rssl@gmail.com>
2025-07-21 21:32:08 +0200

    remove logic from main that's been moved to PlayingState

--- main.lua
+++ main.lua
@@ -1,9 +1,6 @@
 local love = require("love")
 local push = require("push")
-local Bird = require("Bird")
-local PipePair = require("PipePair")
 local keypressed = require("keypressed")
-local Timer = require("Timer")
 local StateMachine = require("StateMachine")
 local PlayingState = require("states.PlayingState")
 
@@ -17,8 +14,6 @@ local WINDOW_HEIGHT = 720
 local BACKGROUND_SCROLL_SPEED = 30
 local BACKGROUND_LOOPING_POINT = 413
 
-local PIPES_SPAWN_DELAY = 2.5
-
 local background_scroll = 0
 local ground_scroll = 0
 
@@ -26,14 +21,6 @@ local ground_scroll = 0
 local background_img
 ---@type love.Image
 local ground_img
-
----@type Bird
-local bird
-
----@type table
-local pipe_pairs = {}
----@type Timer
-local pipe_pairs_spawn_timer
 
 ---@type StateMachine
 local state_machine
@@ -48,9 +35,6 @@ function love.load()
 	push.setupScreen(GAME_WIDTH, GAME_HEIGHT, { upscale = "normal" })
 
 	math.randomseed(os.time())
-
-	bird = Bird:new()
-	pipe_pairs_spawn_timer = Timer:new(PIPES_SPAWN_DELAY)
 
 	state_machine = StateMachine:new({
 		[PlayingState.name] = function()
@@ -72,41 +56,10 @@ function love.keypressed(key, code)
 	end
 end
 
-local function spawn_pipes(dt)
-	pipe_pairs_spawn_timer:update(dt)
-	if pipe_pairs_spawn_timer:finished() then
-		table.insert(pipe_pairs, PipePair:new())
-		pipe_pairs_spawn_timer:reset()
-	end
-end
-
-local function cleanup_pipes()
-	for i, p in ipairs(pipe_pairs) do
-		if p.x + p.width < 0 then
-			table:remove(i)
-		else
-			-- Pipes are ordered so we can break as soon as we encounter an "on-screen" one
-			break
-		end
-	end
-end
-
 function love.update(dt)
 	background_scroll = (background_scroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
 	ground_scroll = (ground_scroll + GAME_SCROLL_SPEED * dt) % GAME_WIDTH
-
-	bird:update(dt)
-
-	cleanup_pipes()
-	spawn_pipes(dt)
-
-	for _, p in ipairs(pipe_pairs) do
-		p:update(dt)
-		if p:collides_with(bird) then
-			state_machine:change(HighscoreState.name)
-			love.event.quit()
-		end
-	end
+	state_machine:update(dt)
 end
 
 function love.draw()
@@ -114,12 +67,7 @@ function love.draw()
 
 	love.graphics.draw(background_img, 0 - background_scroll, 0)
 	love.graphics.draw(ground_img, 0 - ground_scroll, GAME_HEIGHT - ground_img:getHeight())
-
-	bird:render()
-
-	for _, p in ipairs(pipe_pairs) do
-		p:render()
-	end
+	state_machine:render()
 
 	push.finish()
 end

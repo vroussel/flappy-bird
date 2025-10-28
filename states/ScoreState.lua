ba03c31e72ab31946eec7078b7948c8147b4566a
Valentin Roussel <valentin.rssl@gmail.com>
2025-10-28 08:25:58 +0100

    add delay before ability to play again, to avoid missclick

--- states/ScoreState.lua
+++ states/ScoreState.lua
@@ -1,8 +1,10 @@
 local State = require("State")
 local keypressed = require("keypressed")
+local Timer = require("Timer")
 
 ---@class ScoreState: State
 ---@field score number
+---@field show_play_again_timer Timer
 local ScoreState = State:new({
 	name = "score",
 })
@@ -13,6 +15,7 @@ function ScoreState:new()
 	self.__index = self
 
 	s.score = nil
+	s.show_play_again_timer = Timer:new(1)
 
 	return s
 end
@@ -21,10 +24,13 @@ function ScoreState:enter(params)
 	self.score = params.score
 end
 
-function ScoreState:update()
-	if keypressed.pressed("enter") or keypressed.pressed("return") or keypressed.mouse_pressed(1) then
-		if self.state_machine then
-			self.state_machine:change(CountdownState.name)
+function ScoreState:update(dt)
+	self.show_play_again_timer:update(dt)
+	if self.show_play_again_timer:finished() then
+		if keypressed.pressed("enter") or keypressed.pressed("return") or keypressed.mouse_pressed(1) then
+			if self.state_machine then
+				self.state_machine:change(CountdownState.name)
+			end
 		end
 	end
 end
@@ -38,20 +44,22 @@ function ScoreState:render()
 		"center"
 	)
 
-	local action
-	if RunningFromWeb() then
-		action = "Click"
-	else
-		action = "Press Enter"
+	if self.show_play_again_timer:finished() then
+		local action
+		if RunningFromWeb() then
+			action = "Click"
+		else
+			action = "Press Enter"
+		end
+		love.graphics.setFont(Fonts.medium)
+		love.graphics.printf(
+			action .. " to play again",
+			0,
+			GAME_HEIGHT / 3 + Fonts.huge:getHeight() + 10,
+			GAME_WIDTH,
+			"center"
+		)
 	end
-	love.graphics.setFont(Fonts.medium)
-	love.graphics.printf(
-		action .. " to play again",
-		0,
-		GAME_HEIGHT / 3 + Fonts.huge:getHeight() + 10,
-		GAME_WIDTH,
-		"center"
-	)
 end
 
 return ScoreState
